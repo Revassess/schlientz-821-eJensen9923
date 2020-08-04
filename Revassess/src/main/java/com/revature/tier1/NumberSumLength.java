af39abca4130b6d502ebf878b351cd9fc29a853a
Eric Jensen <ejensen9923@mail.kvcc.edu>
2020-08-04 14:51:17 -0400

    finished tier1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,19 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		char[] digits = Long.toString(num).toCharArray();
+		long sum = 0;
+		for(char c: digits) {
+			sum += powerOf(Integer.parseInt(Character.toString(c)),digits.length);
+		}
+		
+		return (num == sum);
+	}
+	
+	private static int powerOf(int in, int length) {
+		int out = 1;
+		for(int i = 0; i < length; i++)
+			out *= in;
+		return out;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,12 @@
 package com.revature.tier1;
 
 public class User {
+	public Integer id = 0;
+	public String firstName = null;
+	public String lastName = null;
+	public String username = null;
+	public String password = null;
+	public String role = null;
+	
+	
 }
