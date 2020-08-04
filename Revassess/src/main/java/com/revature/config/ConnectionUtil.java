944d702c18a888ad4448a25d6b05588f2fafc498
Eric Jensen <ejensen9923@mail.kvcc.edu>
2020-08-04 16:55:14 -0400

    finished tier3

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,15 +16,15 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:oracle:thin:@ejensendb.cksn5iwir3rz.us-east-2.rds.amazonaws.com:1521:ORCL";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "ejensen";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "cos(PI)=-1";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "INCREMENT_BY_THREE";
 
 	// implement this method to connect to the db and return the connection object
 	public Connection connect(){
